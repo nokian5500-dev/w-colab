40dbacf44d026853500892c2b2ebb6b883167368
nokian5500-dev <129507025+nokian5500-dev@users.noreply.github.com>
2023-06-26 16:21:31 +0300

    Update ng.sh

--- ng.sh
+++ ng.sh
@@ -4,7 +4,7 @@ echo "Download ngrok"
 echo "======================="
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
-wget -O ng.sh 
+wget -O ng.sh https://raw.githubusercontent.com/nokian5500-dev/w-colab/main/ng.sh > /dev/null 2>&1
 ./ngrok authtoken 2Rk6SooaDaU3eAg38Do6nrGzCR6_7coiHGLAhZyGX59WzFSZm
 
 ./ngrok 2Rk6SooaDaU3eAg38Do6nrGzCR6_7coiHGLAhZyGX59WzFSZm
