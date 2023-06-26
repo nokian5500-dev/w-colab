52943de32df6df0ad2231aa3f361ed5b47c73131
nokian5500-dev <129507025+nokian5500-dev@users.noreply.github.com>
2023-06-26 15:34:13 +0300

    Update and rename myng.sh to ng.sh

---
+++ ng.sh
@@ -0,0 +1,7 @@
+rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
+echo "======================="
+echo "Download ngrok"
+echo "======================="
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok.zip > /dev/null 2>&1
+./ngrok authtoken 2Rk6SooaDaU3eAg38Do6nrGzCR6_7coiHGLAhZyGX59WzFSZm
