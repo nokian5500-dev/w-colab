ed62f633afe7b44c6cb1463776158c5a285143fb
nokian5500-dev <129507025+nokian5500-dev@users.noreply.github.com>
2023-06-27 15:32:58 +0300

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -2,7 +2,7 @@ echo "======================="
 echo "Download ngrok"
 echo "======================="
 rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
-wget https://github.com/nokian5500-dev/w-colab/archive/refs/heads/main.zip -O main.zip && unzip main.zip > /dev/null 2>&1
+wget -O ng.sh https://raw.githubusercontent.com/nokian5500-dev/w-colab/main/GC/GCngrok.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 clear
@@ -15,5 +15,6 @@ echo "ap - Asia/Pacific (Singapore)"
 echo "au - Australia (Sydney)"
 echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
-./ngrok tcp --region "us" 3389 &>/dev/null &
+set CRP = eu
+./ngrok tcp --region $CRP 3389 &>/dev/null &
 echo "======================="
